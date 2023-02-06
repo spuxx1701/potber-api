0c1ea9c3f05b13b8da10d3eec41acb99da9baf77
Spuxx <ameisenfutter@outlook.com>
2023-02-06 20:40:08 +0100

    configured env variables

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,17 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
   @Get()
   getHello(): string {
-    return 'Hello World!';
+    return JSON.stringify({
+      title: 'potber API',
+      description: 'A RESTful API for foru.mods.de',
+      author: {
+        name: 'Leopold Hock',
+        website: 'https://www.leopoldhock.de',
+        github: 'https://www.github.com/spuxx1701',
+      },
+      github: 'https://www.github.com/potber-api',
+      client: process.env.CLIENT_URL,
+      documentation: process.env.SWAGGER_URL,
+    });
   }
 }
