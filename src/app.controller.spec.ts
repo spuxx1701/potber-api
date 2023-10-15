aa436459e1fb9ab8e06443d42af9b5c4b0f14acb
spuxx1701 <hi@spuxx.dev>
2023-10-15 12:11:53 +0200

    test: app routes are now being tested end-to-end

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,18 +1,35 @@
+import { createTestContainer } from 'test/helpers/create-test-container';
 import { AppController } from './app.controller';
+import { fakeGetRequest } from 'test/helpers/fake-requests';
+import { INestApplication } from '@nestjs/common';
 
 describe('App | AppController', () => {
-  let appController: AppController;
-
-  beforeEach(() => {
-    appController = new AppController();
+  let app: INestApplication;
+  beforeEach(async () => {
+    const container = await createTestContainer({
+      controllers: [AppController],
+      enableEndToEnd: true,
+    });
+    app = container.app;
   });
 
-  describe('index', () => {
-    it('should return some information about the api', () => {
-      const actual = appController.index();
-      expect(actual).toHaveProperty('title');
-      expect(actual).toHaveProperty('description');
-      expect(actual).toHaveProperty('documentation');
+  describe('GET /', () => {
+    it('should return some information about the api', async () => {
+      const request = fakeGetRequest(app, '/');
+      const response = await request.send();
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toHaveProperty('title');
+      expect(response.body).toHaveProperty('description');
+      expect(response.body).toHaveProperty('documentation');
+    });
+  });
+
+  describe('GET /healthz', () => {
+    it('should return the status of the api', async () => {
+      const request = fakeGetRequest(app, '/healthz');
+      const response = await request.send();
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toStrictEqual({ status: 'ok' });
     });
   });
 });
