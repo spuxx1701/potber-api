d88e92bf0ec13a5beb345646d2d8db4032083174
spuxx1701 <hi@spuxx.dev>
2023-12-30 13:52:54 +0100

    feat: Increased the timeout value for outgoing requests to 30 seconds

--- src/http/http.config.ts
+++ src/http/http.config.ts
@@ -3,7 +3,7 @@ import { Logger } from '@nestjs/common';
 import { InternalAxiosRequestConfig } from 'axios';
 
 export const httpConfig: HttpModuleOptions = {
-  timeout: 10000,
+  timeout: 30000,
 };
 
 export function httpRequestInterceptor(
